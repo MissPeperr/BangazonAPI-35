876b3832a66e050e5d0174d3f46cbb0e0f2df569
Madi Peper <madisonpeper@gmail.com>
2020-01-05 14:25:37 -0600

    added all class files;

---
+++ BangazonAPI/Controllers/CustomersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+
+        public CustomersController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT 
+                                        Id, 
+                                        FirstName, 
+                                        LastName, 
+                                        Active, 
+                                        CreatedDate,
+                                        Address,
+                                        City,
+                                        State,
+                                        Email,
+                                        Phone
+                                        FROM Customer";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Customer> allCustomers = new List<Customer>();
+
+                    while (reader.Read())
+                    {
+                        Customer cust = new Customer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            Active = reader.GetBoolean(reader.GetOrdinal("Active")),
+                            CreatedDate = reader.GetDateTime(reader.GetOrdinal("CreatedDate")),
+                            Address = reader.GetString(reader.GetOrdinal("Address")),
+                            City = reader.GetString(reader.GetOrdinal("City")),
+                            State = reader.GetString(reader.GetOrdinal("State")),
+                            Email = reader.GetString(reader.GetOrdinal("Email")),
+                            Phone = reader.GetString(reader.GetOrdinal("Phone"))
+                        };
+
+                        allCustomers.Add(cust);
+                    }
+                    reader.Close();
+
+                    return Ok(allCustomers);
+                }
+            }
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Customers
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
