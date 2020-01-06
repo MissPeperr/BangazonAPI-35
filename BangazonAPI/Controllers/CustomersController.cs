1c17ebb9185e5b17f71dc904562821fdacd7539c
Madi Peper <madisonpeper@gmail.com>
2020-01-06 14:05:32 -0600

    can get a single customer with their product(s)

--- BangazonAPI/Controllers/CustomersController.cs
+++ BangazonAPI/Controllers/CustomersController.cs
@@ -143,6 +143,101 @@ namespace BangazonAPI.Controllers
                 }
             }
         }
+        [HttpGet("{id}", Name = "GetCustomer")]
+
+        public async Task<IActionResult> Get([FromRoute] int id, [FromQuery] string include)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT 
+                                        Id, 
+                                        FirstName, 
+                                        LastName, 
+                                        Active, 
+                                        CreatedDate,
+                                        Address,
+                                        City,
+                                        State,
+                                        Email,
+                                        Phone
+                                        FROM Customer
+                                        WHERE 1=1 AND Active = 1";
+
+                    if (include == "products")
+                    {
+                        cmd.CommandText = @"SELECT 
+                                            c.Id,
+                                            c.FirstName, 
+                                            c.LastName, 
+                                            c.Active, 
+                                            c.CreatedDate,
+                                            c.[Address],
+                                            c.City,
+                                            c.[State],
+                                            c.Email,
+                                            c.Phone,
+                                            p.Id AS ProdId,
+                                            p.Title,
+                                            p.[Description],
+                                            p.Price,
+                                            p.DateAdded,
+                                            p.ProductTypeId,
+                                            pt.Name,
+                                            pt.Id AS ProdTypeId
+                                            FROM Customer c
+                                            LEFT JOIN Product p ON p.CustomerId = c.Id
+                                            LEFT JOIN ProductType pt ON pt.Id = p.ProductTypeId
+                                            WHERE 1 = 1 AND c.Active = 1 AND c.Id = @Id";
+                    }
+                    cmd.Parameters.Add(new SqlParameter("@Id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    Customer cust = null;
+
+                    while (reader.Read())
+                    {
+                        cust = new Customer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            Active = reader.GetBoolean(reader.GetOrdinal("Active")),
+                            CreatedDate = reader.GetDateTime(reader.GetOrdinal("CreatedDate")),
+                            Address = reader.GetString(reader.GetOrdinal("Address")),
+                            City = reader.GetString(reader.GetOrdinal("City")),
+                            State = reader.GetString(reader.GetOrdinal("State")),
+                            Email = reader.GetString(reader.GetOrdinal("Email")),
+                            Phone = reader.GetString(reader.GetOrdinal("Phone")),
+                            Products = new List<Product>()
+                        };
+                        if (include == "products" && ProductExists(reader, reader.GetOrdinal("ProdId")))
+                        {
+                            Product newProduct = new Product()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("ProdId")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                Description = reader.GetString(reader.GetOrdinal("Description")),
+                                Price = reader.GetSqlMoney(reader.GetOrdinal("Price")).ToDouble(),
+                                DateAdded = reader.GetDateTime(reader.GetOrdinal("DateAdded")),
+                                ProductTypeId = reader.GetInt32(reader.GetOrdinal("ProductTypeId")),
+                                ProductType = new ProductType()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("ProdTypeId")),
+                                    Name = reader.GetString(reader.GetOrdinal("Name"))
+                                }
+
+                            };
+                            cust.Products.Add(newProduct);
+                        }
+                    }
+                    reader.Close();
+
+                    return Ok(cust);
+                }
+            }
+        }
         private static bool ProductExists(SqlDataReader reader, int colIndex)
         {
             if (!reader.IsDBNull(colIndex))
